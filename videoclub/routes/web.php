7d637476456240f74886e2331e0807cd94cba92a
AdRosales <a00512067@itesm.mx>
2017-02-23 13:54:16 -0600

    project_videoclub

---
+++ videoclub/routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('login', function () {
+    return view('auth.login');
+});
+
+Route::get('catalog', function () {
+    return view('catalog.index');
+});
+
+Route::get('catalog/show/{id}', function () {
+    return view('catalog.show');
+});
+
+Route::get('catalog/create', function () {
+    return view('catalog.create');
+});
+
+Route::get('catalog/edit/{id}', function () {
+    return view('catalog.edit');
+});
+
+return view('home');
+return view('catalog.index');
+return view('catalog.show', array('id'=>$id));
